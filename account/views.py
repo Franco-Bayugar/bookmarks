9c223c17df585d955598d4c0ddddfcec264bda3e
Franco Bayugar <francobayugardev@gmail.com>
2024-05-14 16:26:32 -0300

    w10 venv created

--- account/views.py
+++ account/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth import logout
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
 
 @login_required
 def user_logout(request):
